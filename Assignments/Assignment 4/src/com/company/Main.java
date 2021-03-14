d7789b1d70663605e0dded43a31fd1cff83c9c78
jspagna4 <jspagna4@gmail.com>
2021-03-13 19:45:35 -0500

    Submit Assignment 4

--- Assignments/Assignment 4/src/com/company/Main.java
+++ Assignments/Assignment 4/src/com/company/Main.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class Main {
 
+    public static int summation(int[] numList) {
+        int sum = 0;
+        for(int i = 0; i < numList.length; i++)
+            sum += numList[i];
+        return sum;
+    }
     public static void main(String[] args) {
-	
+        int[] nums = {1,2,3,4,5};
+        System.out.println(summation(nums));
     }
 }
